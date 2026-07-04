8305ed0d31d906d995a3e66d0a3be659ecdb3ab6
Souvik Pan <souvikpan2006@gmail.com>
2026-07-04 12:24:39 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api/admin', adminRoutes);
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'Online', system: 'MoonWitch Core Architecture' });
+});
+
+module.exports = app;
